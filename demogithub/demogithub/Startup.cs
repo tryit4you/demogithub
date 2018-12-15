92d45b963f1e7a2bc01407751889692c854ecb51
vohung <vohung.it@gmail.com>
2018-12-15 15:40:31 +0700

    change startup

--- demogithub/demogithub/Startup.cs
+++ demogithub/demogithub/Startup.cs
@@ -15,6 +15,7 @@ namespace demogithub
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,6 +30,7 @@ namespace demogithub
             {
                 await context.Response.WriteAsync("Hello World!");
             });
+            app.UseMvc();
         }
     }
 }
